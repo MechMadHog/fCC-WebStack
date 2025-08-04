66e2d4d0682fdf79083bea3bd5b947ffe7c2e8c8
Mike MacDonagh <michael@mechmadhog.com>
2025-08-04 22:20:03 +0100

    Started the fourth
    Registration Form, Tidied folder Structure, Moved Comments from code to Markdown file

---
+++ 02-js-algorithms/Legacy-JavaScript-Curriculum/1.1-Basic-JavaScript.js
@@ -0,0 +1,37 @@
+var myName;
+
+var myFirstName = "Michael";
+var myFirstName = "Mike";
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
+
+// Setup
+var a;
+a = 7;
+var b;
+// Only change code below this line
+b = a;
+
+var a = 9;
+
+var myFirstName = "Mike";
+var myLastName = "MacDonagh";
+
+var properCamelCase;
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+
+
+
