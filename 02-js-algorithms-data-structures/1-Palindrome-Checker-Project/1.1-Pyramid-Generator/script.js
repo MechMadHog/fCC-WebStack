8aaca91d34297af143df06c63b5f7b13cd0f1054
Mike MacDonagh <michael@mechmadhog.com>
2025-09-03 21:31:35 +0100

    I have now began JS from scratch

---
+++ 02-js-algorithms-data-structures/1-Palindrome-Checker-Project/1.1-Pyramid-Generator/script.js
@@ -0,0 +1,11 @@
+/* console.log("Let's build a pyramid using JavaScript basics:"); */
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+
+
+
+console.log(rows);
