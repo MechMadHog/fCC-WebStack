7a5da6d565c220c1f000a4423742c77f707ca8c9
Mike MacDonagh <michael@mechmadhog.com>
2025-09-15 18:28:35 +0100

    15 more

--- 02-js-algorithms-data-structures/1-Palindrome-Checker-Project/1.3-Role-Playing-Game/script.js
+++ 02-js-algorithms-data-structures/1-Palindrome-Checker-Project/1.3-Role-Playing-Game/script.js
@@ -20,24 +20,17 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 const weapons = [
-  {
-    name: "stick",
-    power: 5
-  },
-  {
-    name: "dagger",
-    power: 30
-  },
-  {
-    name: "claw hammer",
-    power: 50
-  },
-  {
-    name: "sword",
-    power: 100
-  }
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
 ];
 
+const monsters = [
+  { name: 'slime', level: 2, health: 15 },
+  { name: 'fanged beast', level: 8, health: 60 },
+  { name: 'dragon', level: 20, health: 300 }
+];
 
 const locations = [
   {
@@ -58,6 +51,12 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
   },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
+  }
 ];
 
 const cat = {
@@ -94,10 +93,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
 function buyHealth() {
   if(gold >= 10){
   gold -= 10;
@@ -124,11 +119,43 @@ function buyWeapon() {
     }
   } else {
       text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
 function fightSlime() {
+  fighting = 0;
+  goFight();
 }
 
-function fightBeast() {   
+function fightBeast() {
+  fighting = 1;
+  goFight();  
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight(); 
+}
+
+function goFight() {   
+}
+
+function attack() {   
+}
+
+function dodge() {   
 }
