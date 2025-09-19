02910b31c3e1f951d66a4e0c965f7a13a6f69557
Mike MacDonagh <michael@mechmadhog.com>
2025-09-19 20:39:08 +0100

    Rock, Paper, Scissors (started)

---
+++ 02-js-algorithms-data-structures/1-Palindrome-Checker-Project/1.6-Rock-Paper-Scissors-Game/script.js
@@ -0,0 +1,31 @@
+function getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  const playerWon = hasPlayerWonTheRound(userOption, computerResult);
+  if(playerWon) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`
+  }
+ if(computerResult == userOption) {
+    return `It's a tie! Both chose ${userOption}`
+ }
+}
+
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
