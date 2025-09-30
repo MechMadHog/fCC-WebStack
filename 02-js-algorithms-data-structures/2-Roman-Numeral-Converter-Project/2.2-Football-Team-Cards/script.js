3da81212408722bc3afe27577c64037baff9ade2
Mike MacDonagh <michael@mechmadhog.com>
2025-09-30 18:39:09 +0100

    Update script.js

--- 02-js-algorithms-data-structures/2-Roman-Numeral-Converter-Project/2.2-Football-Team-Cards/script.js
+++ 02-js-algorithms-data-structures/2-Roman-Numeral-Converter-Project/2.2-Football-Team-Cards/script.js
@@ -177,7 +177,6 @@ const {coachName} = myFavoriteFootballTeam.headCoach;
 
 typeOfSport.textContent = sport;
 teamName.textContent = team;
-
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
