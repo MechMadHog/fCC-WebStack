7cb20b657cb2a88683ee20d87fb35243fa47c5c3
Mike MacDonagh <michael@mechmadhog.com>
2025-10-03 15:28:40 +0100

    another 12

--- 02-js-algorithms-data-structures/2-Roman-Numeral-Converter-Project/2.3-Todo-App/script.js
+++ 02-js-algorithms-data-structures/2-Roman-Numeral-Converter-Project/2.3-Todo-App/script.js
@@ -13,49 +13,72 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  updateTaskContainer();
+  reset();
+};
+
+const updateTaskContainer = () => {
+    tasksContainer.innerHTML = "";
+    taskData.forEach(
+    ({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+        </div>
+      `
+    }
+  );
+};
+
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+}
+
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+}
+
 openTaskFormBtn.addEventListener("click", () =>
-    taskForm.classList.toggle("hidden")
+  taskForm.classList.toggle("hidden")
 );
 
-closeTaskFormBtn.addEventListener("click", () =>
-    confirmCloseDialog.showModal()
-);
+closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  if (formInputsContainValues) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
+});
 
-cancelBtn.addEventListener("click", () =>
-    confirmCloseDialog.close()
-);
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close()
-    taskForm.classList.toggle("hidden")
+  confirmCloseDialog.close();
+  reset()
 });
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-    };
-    if(dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
-    taskData.forEach(({id, title, date, description}) => {
-        tasksContainer.innerHTML+= 
-        `<div class="task" id="${id}">
-            <p><strong>Title:</strong> ${title}</p>
-            <p><strong>Date:</strong> ${date}</p>
-            <p><strong>Description:</strong> ${description}</p>
-            <button type="button" class="btn">Edit</button>
-            <button type="button" class="btn">Delete</button>
-        </div>`
-    });
-    taskForm.classList.toggle("hidden");
+    addOrUpdateTask();
 });
-
-const reset = () => {
-    
-}
-
