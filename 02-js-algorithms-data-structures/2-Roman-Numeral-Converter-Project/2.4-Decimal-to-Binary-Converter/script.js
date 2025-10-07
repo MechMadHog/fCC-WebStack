70c0634d5b8a2b41fcf44fc1182df0a6c2c880c8
Mike MacDonagh <michael@mechmadhog.com>
2025-10-07 19:14:24 +0100

    75 done

--- 02-js-algorithms-data-structures/2-Roman-Numeral-Converter-Project/2.4-Decimal-to-Binary-Converter/script.js
+++ 02-js-algorithms-data-structures/2-Roman-Numeral-Converter-Project/2.4-Decimal-to-Binary-Converter/script.js
@@ -5,13 +5,15 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-    if(input === 0){
-        return "0";
-    } else if (input === 1){
-        return "1";
+    if(input === 0 || input === 1){
+        return String(input);
     } else {
         return decimalToBinary(Math.floor(input / 2)) + (input % 2);
     }
+};
+
+const showAnimation = () => {
+
 };
 
 const checkUserInput = () => {
@@ -19,6 +21,11 @@ const checkUserInput = () => {
         alert("Please provide a decimal number greater than or equal to 0");
         return;
     }
+
+    if (parseInt(numberInput.value) === 5) {
+        
+    }
+
     result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = "";
 };
